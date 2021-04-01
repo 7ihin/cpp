f9546d1e3cad8f81a7c6d2b063870c10c1006cd1
권민재 <80219265+7ihin@users.noreply.github.com>
2021-04-01 13:35:46 +0900

    Update 3주차과제.cpp

--- 3주차과제.cpp
+++ 3주차과제.cpp
@@ -1,55 +1,57 @@
-﻿#include<iostream>
-#include<string>
+#include <iostream>
 
 using namespace std;
 
-struct NODE     // 자기참조 구조체, 변수정의
+struct node
 {
-	string name;
-	int id;
-	float salary;
-	NODE* p_next;
+    string name;
+    int id = 0;
+    float salary = 0;
+    struct node* p_next = 0;
 };
 
+node head;
+node node1, node2, node3, node4;
 
-void input_node()     // 정보입력
+void input_node()
 {
-	struct NODE* node1;
-	node1->name = "Student1";
-	node1->id = 1;
-	node1->salary = 1;
+    node1.name = "권민재";
+    node1.id = 202131401;
+    node1.salary = 1;
+    head.p_next = &node1;
 
+    node2.name = "최재혁";
+    node2.id = 202131421;
+    node2.salary = 2;
+    node1.p_next = &node2;
 
-	struct NODE* node2;
-	node2->name = "Student2";
-	node2->id = 2;
-	node2->salary = 2;
+    node3.name = "윤호준";
+    node3.id = 202131464;
+    node3.salary = 3;
+    node2.p_next = &node3;
 
-	node1->p_next = node2;
-
-	struct NODE* node3;
-	node3->name = "Student3";
-	node3->id = 3;
-	node3->salary = 3;
-
-	node2->p_next = node3;
-
-	struct NODE* node4;
-	node4->name = "Student4";
-	node4->id = 4;
-	node4->salary = 4;
-
-	node3->p_next = node4;
-	node4->p_next = NULL;
+    node4.name = "박준혁";
+    node4.id = 202131434;
+    node4.salary = 4;
+    node4.p_next = NULL;
+    node3.p_next = &node4;
 }
 
-void print_node()     // 출력
+void print_node()
 {
-	struct NODE head
-	struct NODE* curr = head->p_next;
-	while (curr != NULL)
-	{
-		printf("Name: %s\n ID: %d\n Salary: %f\n", curr->name, curr->id, curr->salary);
-		curr = curr->p_next;
-	}
+    node* curr = head.p_next;
+    while (curr != NULL)
+    {
+        cout << curr->name << endl; 
+        cout << curr->id << endl;
+        cout << curr->salary << endl;
+        curr = curr->p_next;
+    }
+}
+
+int main()
+{
+    input_node();
+    print_node();
+    return 0;
 }
